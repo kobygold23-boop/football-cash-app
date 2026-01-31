67cf0cdab1ff320d44274e12941c0522b3cba589
kobygold23-boop <kobygold23@gmail.com>
2026-01-31 12:10:24 +0000

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
 const player = document.getElementById("player");
+const pitch = document.getElementById("pitch");
 
 let x = 135;
 let y = 430;
-const step = 10;
+const speed = 10;
 
 function updatePlayer() {
   player.style.left = x + "px";
@@ -10,27 +11,21 @@ function updatePlayer() {
 }
 
 function moveUp() {
-  y -= step;
-  if (y < 0) y = 0;
+  if (y > 0) y -= speed;
   updatePlayer();
 }
 
 function moveDown() {
-  y += step;
-  if (y > 470) y = 470;
+  if (y < pitch.clientHeight - player.clientHeight) y += speed;
   updatePlayer();
 }
 
 function moveLeft() {
-  x -= step;
-  if (x < 0) x = 0;
+  if (x > 0) x -= speed;
   updatePlayer();
 }
 
 function moveRight() {
-  x += step;
-  if (x > 270) x = 270;
+  if (x < pitch.clientWidth - player.clientWidth) x += speed;
   updatePlayer();
 }
-
-updatePlayer();
