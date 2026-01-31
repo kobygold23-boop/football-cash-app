e9bd6498478e64c3de434c0a8c30602adf1238e9
kobygold23-boop <kobygold23@gmail.com>
2026-01-31 12:05:01 +0000

    Update game.js

--- game.js
+++ game.js
@@ -4,12 +4,33 @@ let x = 135;
 let y = 430;
 const step = 10;
 
-document.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowUp") y -= step;
-  if (e.key === "ArrowDown") y += step;
-  if (e.key === "ArrowLeft") x -= step;
-  if (e.key === "ArrowRight") x += step;
-
+function updatePlayer() {
   player.style.left = x + "px";
   player.style.top = y + "px";
-});
+}
+
+function moveUp() {
+  y -= step;
+  if (y < 0) y = 0;
+  updatePlayer();
+}
+
+function moveDown() {
+  y += step;
+  if (y > 470) y = 470;
+  updatePlayer();
+}
+
+function moveLeft() {
+  x -= step;
+  if (x < 0) x = 0;
+  updatePlayer();
+}
+
+function moveRight() {
+  x += step;
+  if (x > 270) x = 270;
+  updatePlayer();
+}
+
+updatePlayer();
